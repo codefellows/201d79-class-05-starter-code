5788e0c5d8716e115f9916648320e281cdaf046e
Minhang Xie <minxie97@gmail.com>
2021-09-17 16:42:15 -0700

    sumAndMultiply question

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function sum(a, b) { //eslint-disable-line
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// testSum(4, 7);
 
 //Jacob did question 1 for the class
 
@@ -44,7 +44,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -62,10 +62,20 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 
+  let sumAB = sum(a, b)
+  let sumTotal = sum(sumAB[0], c)
+  let prodAB = multiply(a, b)
+  let multiplyTotal = multiply(prodAB[0], c)
+
+  let result = [sumTotal[0], multiplyTotal[0], a + ' and ' + b + ' and ' + c + ' sum to ' + sumTotal[0] + '.',
+  'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiplyTotal[0] + '.']
+
+  console.log(result);
+  return result;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
