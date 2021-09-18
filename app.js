51bc29295f4d1a6421c4b1675bda0760484083c9
Samuel Panek <samuelrpanek@gmail.com>
2021-09-17 18:04:15 -0700

    finishes problem-5

--- app.js
+++ app.js
@@ -117,12 +117,17 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
+function multiplyArray(multArr) { 
+  let arrayMx = multiply(multArr[0],multArr[1]);
+  let secondMx = multiply(arrayMx[0],multArr[2]);
+  let result = [secondMx[0], "The numbers " + multArr[0] + "," + multArr[1] + "," + multArr[2] + " have a product of " + secondMx[0] + '.'];
+  console.log(result);
+  return result;
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
