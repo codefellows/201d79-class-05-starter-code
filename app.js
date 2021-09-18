02a31f7086386049f25f5d2c8abf1e9a896bc554
Edris Berg <ali_berg1@yahoo.com>
2021-09-17 18:58:00 -0700

    problem-05-05

--- app.js
+++ app.js
@@ -125,10 +125,21 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
 
+  let firstElement = testArray[0];
+  let SecElement = testArray[1];
+  let thirdElement = testArray[2];
+
+  let multiple = multiply(firstElement, SecElement)[0];
+  let totalMultiple = multiply(thirdElement, multiple)[0];
+  //"The numbers 2,3,4 have a product of 24."
+  let multiArray = [totalMultiple, 'The numbers ' + firstElement + ',' + SecElement + ',' + thirdElement + ' have a product of 24.'];
+
+  return multiArray;
+
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
