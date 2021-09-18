6ad6a7606e88f3fd44c3915353b21fa57cceff59
3lueHippo <3lueHippo@users.noreply.github.com>
2021-09-17 19:53:37 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
+
 function sum(a, b) { //eslint-disable-line
 
-  let total = a + b;
+  let varTotal = a + b;
 
-  let result = [total, 'The sum of ' + a + ' and ' + b + ' is ' + total + '.'];
+  let result = [varTotal, 'The sum of ' + a + ' and ' + b + ' is ' + varTotal + '.'];
 
   console.log(result);
   return result;
@@ -88,11 +89,20 @@ let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
+
+  let first = sum(testArray[0],testArray[1])[0]
+  console.log(first)
+  let total = sum(first, testArray[2])[0]
+
+let final = (testArray +' was passed in as an array of numbers, and ' + total + ' is their sum.')
+let result = [total, final]
+console.log(result);
+  return result;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
