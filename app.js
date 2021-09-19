d00300b601fa01c288c934b17f11bdab6dda916b
KLan Stovall <kland13@gmail.com>
2021-09-19 15:16:36 -0700

    Lab5

--- app.js
+++ app.js
@@ -83,12 +83,13 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let totalSum = sumAndMultiply(sumArr[0], sumArr[1], sumArr[2])[0];
+  return [totalSum, `2,3,4 was passed in as an array of numbers, and ${totalSum} is their sum.`];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -104,11 +105,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  let multiplyArray = multiply(multArr[0], multArr[1])
+  multiplyArray = multiply(multiplyArray[0], multArr[2])
+  let multiplyString = `The numbers ${multArr} have a product of ${multiplyArray[0]}.`
 
+  return [multiplyArray[0], multiplyString]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
