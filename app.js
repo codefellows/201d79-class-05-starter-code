8125f89e9362c4d39f6d71e28dc890a9d9b55643
Jordan Fleming <jordano.fleming@gmail.com>
2021-09-19 22:39:43 -0700

    Finished last problem for lab.

--- app.js
+++ app.js
@@ -83,12 +83,14 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let sumAB = sum(2, 3)[0];
+  let totalSum = sum(sumAB, 4)[0];
+  return [totalSum, `2,3,4 was passed in as an array of numbers, and ${totalSum} is their sum.`];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -104,11 +106,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  let productAB = multiply(multArr[0], multArr[1])[0];
+  let totalProduct = multiply(productAB, multArr[2])[0];
+  return [totalProduct, `The numbers 2,3,4 have a product of ${totalProduct}.`];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -132,7 +136,7 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+  
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
